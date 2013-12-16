24347d5a70f0fbd3592158c25653f804a91f78c9
AlexandrosM <alexandros@rulemotion.com>
2013-12-16 00:22:11 +0000

    convert gulpfile to coffeescript

---
+++ gulpfile.coffee
@@ -0,0 +1,29 @@
+# Find plugins at https://npmjs.org/browse/keyword/gulpplugin
+gulp = require 'gulp'
+coffee = require 'gulp-coffee'
+concat = require 'gulp-concat'
+
+gulp.task 'build', ->
+	sink = concat('main.js')
+
+	gulp.src('lib/base64-binary.js')
+		.pipe(sink, end: false)
+
+	gulp.src('src/validateSSH.coffee')
+		.pipe(coffee(bare: true))
+		.pipe(sink)
+
+	sink
+		.pipe(gulp.dest('./dist/'));
+
+# The default task (called when you run `gulp`)
+gulp.task 'default', ->
+	gulp.run 'build'
+
+	# Watch files and run tasks if they change
+	gulp.watch [
+		'lib/base64-binary.js',
+		'src/validateSSH.coffee'
+	], (event) ->
+		gulp.run 'build'
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,6 @@
-/*
- Find plugins at https://npmjs.org/browse/keyword/gulpplugin
- */
-var gulp = require('gulp');
-var coffee = require('gulp-coffee');
-var concat = require('gulp-concat');
+//the build instructions are in gulpfile.coffee
+//this file is a simple bridge since gulp doesn't support
+//coffeescript files natively.
 
-gulp.task('build', function() {
-    var sink = concat("main.js");
-
-    gulp.src('lib/base64-binary.js')
-        .pipe(sink, {end: false});
-
-    gulp.src('src/validateSSH.coffee')
-        .pipe(coffee({bare: true}))
-        .pipe(sink);
-
-    sink.pipe(gulp.dest('./dist/'));
-});
-
-// The default task (called when you run `gulp`)
-gulp.task('default', function() {
-    gulp.run('build');
-
-    // Watch files and run tasks if they change
-    gulp.watch([
-        'lib/base64-binary.js',
-        'src/validateSSH.coffee'
-    ], function(event) {
-        gulp.run('build');
-    });
-});
+require('coffee-script')
+require('./gulpfile.coffee')
